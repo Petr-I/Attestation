47a21ecd69c9002230e2f7bd82e0866ff4ae4b74
Peter Bakeev <bakeev.petya10000@gmail.com>
2023-08-16 23:16:13 +0300

    improved main file

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ if uploaded_file is not None:
 
     numerical = df.dtypes[df.dtypes == 'int64'] + df.dtypes[df.dtypes == 'float64']
 
-    dropdown1 = st.selectbox(
+    dropdown_1 = st.selectbox(
         'First column',
         (df.columns.tolist()))
 
-    if dropdown1 in numerical:
-        fig1 = px.histogram(df, x=dropdown1)
+    if dropdown_1 in numerical:
+        fig1 = px.histogram(df, x=dropdown_1)
     else:
-        fig1 = px.pie(df, names=dropdown1)
+        fig1 = px.pie(df, names=dropdown_1)
 
     st.plotly_chart(fig1, use_container_width=True)
 
-    dropdown2 = st.selectbox(
+    dropdown_2 = st.selectbox(
         'Second column',
         (df.columns.tolist()))
 
-    if dropdown2 in numerical:
-        fig2 = px.histogram(df, x=dropdown2)
+    if dropdown_2 in numerical:
+        fig2 = px.histogram(df, x=dropdown_2)
     else:
-        fig2 = px.pie(df, names=dropdown2)
+        fig2 = px.pie(df, names=dropdown_2)
 
     st.plotly_chart(fig2, use_container_width=True)
 
@@ -44,18 +44,42 @@ if uploaded_file is not None:
         testing_methods
     )
 
-    if testing_dd == testing_methods[0]:
-        st.write('Select testing method')
-    elif testing_dd == testing_methods[1] and (dropdown1 and dropdown2) in numerical:
-        res = stats.ttest_ind(
-            df[dropdown1], df[dropdown2],
-            equal_var=False
+    categor = []
+    st.write(type(df.columns.values.tolist()))
+    for i in df.columns.values.tolist():
+        if i in numerical:
+            continue
+        else:
+            categor.append(i)
+
+    categor_column = st.selectbox(
+        'Select a categorical column to test the hypothesis',
+        categor
+    )
+
+    categor_perem = st.multiselect(
+        f'Select 2 column values {categor_column}',
+        df[categor_column].unique(), max_selections=2
+    )
+
+    if len(categor_perem) == 2:
+        value_dropdown = st.selectbox(
+            f'Select the column by which to compare {categor_perem[0]} and {categor_perem[1]}',
+            [col for col in df if isinstance(df[col][0], int) or isinstance(df[col][0], float)],
+            key='1'
         )
-        st.write(f'P value: {res.pvalue}')
-    elif testing_dd == testing_methods[2] and (dropdown1 and dropdown2) in numerical:
-        res = stats.mannwhitneyu(
-            df[dropdown1], df[dropdown2]
-        )
-        st.write(f'P value: {res.pvalue}')
-    else:
-        st.write('First or second column (or both) are not numeric')
+        data_1, data_2 = df[df[categor_column] == categor_perem[0]][value_dropdown], df[df[categor_column] == categor_perem[1]][value_dropdown]
+
+        if testing_dd == testing_methods[0]:
+            st.write('Select testing method')
+        elif testing_dd == testing_methods[1]:
+            res = stats.ttest_ind(
+                data_1, data_2,
+                equal_var=False
+            )
+            st.write(f'P value: {res.pvalue}')
+        elif testing_dd == testing_methods[2]:
+            res = stats.mannwhitneyu(
+                data_1, data_2
+            )
+            st.write(f'P value: {res.pvalue}')
